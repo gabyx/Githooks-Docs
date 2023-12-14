dd77a263b0f2ddcef35e473f1cf2ce986e6e6646
Gabriel Nützi <gnuetzi@gmail.com>
2023-12-14 22:11:19 +0100

    fix: Format with prettier 3.1.0

--- githooks/scripts/format-docs-all.sh
+++ githooks/scripts/format-docs-all.sh
@@ -9,47 +9,78 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 . "$DIR/../common/parallel.sh"
 . "$DIR/../common/docs-format.sh"
 
-dir="${1:-}"
-excludeIgnorePath="${2:-}"
-fileGlob=${3:-$(getGeneralDocsFileGlob)}
+dryRun="true"
+dir=""
+excludeIgnorePath=""
+fileGlob=$(getGeneralDocsFileGlob)
+
+function help() {
+    printError "Usage:" \
+        "  [--force]                      : Force the format." \
+        "  [--exclude-ignore-path <path>] : Exclude file ignore path." \
+        "  [--glob-pattern <pattern>]     : Glob pattern format files." \
+        "   --dir <path>                  : In which directory to format files."
+}
+
+function parseArgs() {
+    local prev=""
+
+    for p in "$@"; do
+        if [ "$p" = "--force" ]; then
+            dryRun="false"
+        elif [ "$p" = "--help" ]; then
+            help
+            return 1
+        elif [ "$p" = "--dir" ]; then
+            true
+        elif [ "$prev" = "--dir" ]; then
+            dir="$p"
+        elif [ "$p" = "--exclude-ignore-path" ]; then
+            true
+        elif [ "$prev" = "--exclude-ignore-path" ]; then
+            excludeIgnorePath="$p"
+        elif [ "$p" = "--glob-pattern" ]; then
+            true
+        elif [ "$prev" = "--glob-pattern" ]; then
+            fileGlob="$p"
+        else
+            printError "! Unknown argument \`$p\`"
+            help
+            return 1
+        fi
+
+        prev="$p"
+    done
+}
+
+parseArgs "$@"
 
 [ -d "$dir" ] || die "Directory '$dir' does not exist."
 
-read -r -p "Shall we really format all files? (No, yes, dry run) [N|y|d]: " what
+addArgs=()
 
-dryRun="false"
-
-if [ "$what" = "d" ]; then
-    what="y"
-    dryRun="true"
+if [ "$dryRun" = "false" ]; then
+    assertConfigsFormatVersion "3.0.0" "4.0.0"
+    addArgs+=(--write)
+    printInfo "Formatting in '$dir with '$fileGlob'."
+else
+    printInfo "Dry-run formatting in '$dir' with '$fileGlob'."
+    addArgs+=(--list-different)
 fi
 
-if [ "$what" = "y" ]; then
-    addArgs=()
+if [ -n "$excludeIgnorePath" ]; then
+    [ -f "$excludeIgnorePath" ] || die "Ignore file '$excludeIgnorePath' for prettier does not exist."
 
-    if [ "$dryRun" = "false" ]; then
-        assertDocsFormatVersion "2.8.3" "3.0.0"
-        addArgs+=(--write)
-        printInfo "Formatting in '$dir with '$fileGlob'."
-    else
-        printInfo "Dry-run formatting in '$dir' with '$fileGlob'."
-        addArgs+=(--list-different)
-    fi
-
-    if [ -n "$excludeIgnorePath" ]; then
-        [ -f "$excludeIgnorePath" ] || die "Ignore file '$excludeIgnorePath' for prettier does not exist."
-
-        addArgs+=(--ignore-path "$excludeIgnorePath")
-    fi
-
-    prettierExe="pprettier"
-    if ! command -v "$prettierExe" &>/dev/null; then
-        prettierExe="prettier"
-    fi
-
-    # Format with no config -> search directory tree upwards.
-    (cd "$dir" && "$prettierExe" "${addArgs[@]}" "$fileGlob") ||
-        die "Formatting in '$dir' with '$fileGlob' failed."
+    addArgs+=(--ignore-path "$excludeIgnorePath")
 fi
+
+prettierExe="pprettier"
+if ! command -v "$prettierExe" &>/dev/null; then
+    prettierExe="prettier"
+fi
+
+# Format with no config -> search directory tree upwards.
+(cd "$dir" && "$prettierExe" "${addArgs[@]}" "$fileGlob") ||
+    die "Formatting in '$dir' with '$fileGlob' failed."
 
 exit 0
